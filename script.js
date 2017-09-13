03c55c42993e188c77127a8194d3b689c301292f
Dasun Pubudumal <dasun.15@cse.mrt.ac.lk>
2017-09-13 19:46:18 +0530

    Connection Done.

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var sys = require('util');
 var exec = require('child_process').exec;
-let output;
-
+let output
+let connection_output;
+ // nmcli d wifi connect EEE password '12345678901234567890123456'
 child = exec("nmcli d wifi", function (error, stdout, stderr) {
   // sys.print('stdout: ' + stdout);
   // sys.print('stderr: ' + stderr);
@@ -10,7 +11,7 @@ child = exec("nmcli d wifi", function (error, stdout, stderr) {
   }
   output = stdout;
 
-  console.log(stdout);  //Let us log the output for development purposes.
+  console.log(output);  //Let us log the output for development purposes.
   //TODO: We have to remove this in the production mode.
 });
 
@@ -24,4 +25,18 @@ $('#info').click(function(){
   $('#details_buttons').attr('class', 'animated wobble col-sm-6');
   $('#info_section').attr('class', 'animated slideInRight col-sm-6');
   $('#info_section').css('visibility','visible');
+});
+
+$('#btnSubmit').click(function(){
+  child = exec(("nmcli d wifi connect '" + $('#user_ssid').val() + "' password " + "'" + $('#user_password').val() + "'") , function (error, stdout, stderr) {
+    // sys.print('stdout: ' + stdout);
+    // sys.print('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+    connection_output = stdout;
+
+    console.log(connection_output);  //Let us log the output for development purposes.
+    //TODO: We have to remove this in the production mode.
+  });
 });
